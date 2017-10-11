c77872e96646e99644f5c0c04d423954f5c50fd8
jasonchan03 <jctaurus503@gmail.com>
2017-10-12 00:28:56 +0800

    Added product review table into database

--- database/product_and_product_review.sql
+++ database/product_and_product_review.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2017 at 02:36 PM
--- Server version: 10.1.26-MariaDB
--- PHP Version: 7.1.9
+-- Generation Time: Oct 11, 2017 at 06:27 PM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -53,6 +53,20 @@ INSERT INTO `products` (`product_id`, `product_name`, `product_desc`, `product_d
 (2, 'Ice Cream Soda', 'buymebuymebuymebuyme', '2017-10-11 07:56:34', '100.25', 'must be grey', 'lol idk', 'only in my.', 'stuff', '5.00', 'husky', 'assets/images/products/sample2.jpg', 2),
 (3, 'CAT CAKE', 'this is a lynx. it is the most annoying piece of cat in the world. dont buy, will not refund', '2017-10-11 07:58:54', '50.00', 'colorfull', 'M, L', 'Here', 'no polies', '1.50', 'lynx, evil', 'assets/images/products/sample3.jpg', 0);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_review`
+--
+
+CREATE TABLE `product_review` (
+  `review_id` int(10) NOT NULL,
+  `product_id` int(10) NOT NULL,
+  `userid` varchar(12) NOT NULL,
+  `review_desc` varchar(500) NOT NULL,
+  `review_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
@@ -64,6 +78,14 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`product_id`);
 
 --
+-- Indexes for table `product_review`
+--
+ALTER TABLE `product_review`
+  ADD PRIMARY KEY (`review_id`),
+  ADD KEY `userid` (`userid`),
+  ADD KEY `product_id` (`product_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -72,6 +94,21 @@ ALTER TABLE `products`
 --
 ALTER TABLE `products`
   MODIFY `product_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `product_review`
+--
+ALTER TABLE `product_review`
+  MODIFY `review_id` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `product_review`
+--
+ALTER TABLE `product_review`
+  ADD CONSTRAINT `product_review_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `account` (`userid`),
+  ADD CONSTRAINT `product_review_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
