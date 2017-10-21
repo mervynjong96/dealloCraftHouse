4ba82bd44970bfed83dfa794f8511efe5587b69b
professorgene <werewolf.xeno@yahoo.com>
2017-10-21 21:51:02 +0800

    bootstrap fixes

--- process/list_product.php
+++ process/list_product.php
@@ -16,19 +16,22 @@ function list_product($list) {
 
     if ($list == 1) {
         // sort by new products
-        $products = mysqli_query($conn, "SELECT * FROM products ORDER BY product_date_created DESC LIMIT 4");       
+        $products = mysqli_query($conn, "SELECT * FROM products ORDER BY product_date_created DESC LIMIT 4");
+        $divstyle = "item product col-lg-3 col-md-3 col-sm-5 col-xs-8";
     } else if ($list == 2) {
         // sort by editor's pick
         $products = mysqli_query($conn, "SELECT * FROM products ORDER BY product_quantity_sold DESC LIMIT 4");
+        $divstyle = "item product col-lg-3 col-md-3 col-sm-5 col-xs-8";
     } else {
         $offset = 0;
         $products = mysqli_query($conn, "SELECT * FROM products LIMIT $offset, 12");
+        $divstyle = "item product col-lg-4 col-md-4 col-sm-6 col-xs-12";
     }
 
     if (!empty($products)) {
         foreach($products as $products) { 
 ?>
-            <div class="item product col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            <div class="<?php echo $divstyle ?>">
                 <div class="list-productpicture">
                     <a href="product_details.php?id=<?php echo $products["product_id"]; ?>">
                         <!--
