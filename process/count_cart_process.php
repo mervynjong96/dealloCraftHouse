f84acef263de0b21151d531549519512a61ba04e
jasonchan03 <jctaurus503@gmail.com>
2017-10-28 21:44:03 +0800

    Added count_cart_process.php

---
+++ process/count_cart_process.php
@@ -0,0 +1,34 @@
+<?php
+	$host = "localhost";
+	$user = "root";
+	$pwd = "";
+	$sql_db = "deallocrafthouse";
+	$conn = @mysqli_connect(
+		$host,
+		$user,
+		$pwd,
+		$sql_db
+	);
+
+	session_start();
+
+	// Check if all fields are set with value
+    if( 
+		isset($_SESSION["login_user"])
+	) {
+        $userid = mysqli_real_escape_string($conn, $_SESSION["login_user"]);
+      
+		//Count number of items in the database that matches userid
+		$sql_table = "cart_product";
+		$query = "SELECT COUNT(*) as items_count FROM $sql_table WHERE userid = '$userid'";
+		$result = mysqli_query($conn, $query);
+
+		if(mysqli_affected_rows($conn) > 0) {
+			$result_items = mysqli_fetch_assoc($result);
+			// save it into session and display it on the navigation bar
+			$_SESSION["items_in_cart"]	= $result_items["items_count"];
+		}
+		
+		mysqli_close($conn);
+	}
+?>
