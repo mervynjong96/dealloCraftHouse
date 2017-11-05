7a5af1d41e4d24eac0bcc7f905f49af7a4a829db
jasonchan03 <jctaurus503@gmail.com>
2017-11-06 00:50:00 +0800

    Modified product_details ui

--- product_details.php
+++ product_details.php
@@ -35,6 +35,8 @@
         <div class="content">
             <div class="container">
                 
+				<span id="resultAdd"></span>
+				
                 <div class="well productdetail">
                     <img class="img-responsive" src="
                         <?php
@@ -98,9 +100,13 @@
                         
                         <h3>Price</h3>
                         <p>$<?php echo $product["product_price"]; ?> </p>
-                        
-						<h3>Quantity</h3>
-						<?php
+                        <?php
+							
+							if(isset($_SESSION["login_user"])){?>
+							
+								
+							<h3>Quantity</h3>
+						<p><?php 
 						echo "<div class='input-group'>
 							<span class='input-group-btn'>
 								  <button type='button' class='btn btn-default btn-number' id='minus_button_" . $product['product_id'] . "' onclick='minusQuantity(". $product['product_id'] .",this.id,". $product["product_stockQty"] . ")'>
@@ -113,16 +119,26 @@
 									  <span class='glyphicon glyphicon-plus'></span>
 								  </button>
           					</span>
-						</div>";
-						?>
-						<p><a class="btn btn-success" href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Add to Cart</a></p>
+						</div>
+						<p> Stock left: " . $product['product_stockQty'] . "</p>";
+							?></p>
+									
+								
+									
+								<p><a class="btn btn-success" href="#" onclick=""><span class="glyphicon glyphicon-shopping-cart"></span> Add to Cart</a></p>
+						<?php	}?>
+								
                         <p>Tags: <em><?php echo $product["product_tag"]; ?></em></p>
                     </div>
                 </div>
                 
                 <div class="well">
                     <div class="text-right">
+						 <?php
+							
+							if(isset($_SESSION["login_user"])){?>
                         <a class="btn btn-info">Write a Review</a>
+						<?php } ?>
                     </div>
                     
                     <hr>
@@ -161,53 +177,27 @@
             include_once "./include/Footer.php"
         ?>
 		
-		<script>
-			function sumTotal(value,product_id){
-				if(document.getElementById(product_id).checked){
-					total = (parseFloat(total) + parseFloat(value)).toFixed(2);
-					document.getElementById("total_price").innerHTML=total;
-				}else{
-					total = (parseFloat(total) - parseFloat(value)).toFixed(2);
-					document.getElementById("total_price").innerHTML=total;
-				}
-			}
-			
-			function removeItem(product_id){
-				webix.confirm("Do you want to remove this item fom your cart?", function(result){
-					if(result){
-							webix.ajax().post("process/remove_item_from_cart_process.php",{remove_product_id:product_id},
-							function(text,data){
-								alert(text);
-
-								if(text=="Remove item successfully"){
-									location.reload();
-								}
-							})
-
-					}
-				});
-			}
-			
+		<script>		
 			function minusQuantity(product_id,button_id,maxQuantity){
 				var field_id = "product_quantity_" + product_id;
 				var input_field = $("input[name='"+ field_id + "']");
 				var currentVal = parseInt(input_field.val());
 				//var plus_button = document.getElementById("plus_button_"+product_id);
 								
-				//if(currentVal>1){
+				if(currentVal>1){
 					currentVal--;
 					input_field.val(currentVal);
 					/*if(plus_button.disabled){
 						plus_button.disabled = false;
 					}*/
-				//}
+				}
 				
 				/*if(currentVal <=1){
 					document.getElementById(button_id).disabled = true;
 				}*/
 				
 				//updateInput(field_id,product_id,maxQuantity);
-				runUpdate(product_id,currentVal,maxQuantity);
+				
 			}
 			
 			function addQuantity(product_id,button_id,maxQuantity){
@@ -216,13 +206,13 @@
 				var currentVal = parseInt(input_field.val());
 				var minus_button = document.getElementById("minus_button_"+product_id);
 								
-				//if(currentVal<maxQuantity){
+				if(currentVal<maxQuantity){
 					currentVal++;
 					input_field.val(currentVal);
 					/*if(minus_button.disabled){
 						minus_button.disabled = false;
 					}*/
-				//}
+				}
 				
 				/*if(currentVal >=maxQuantity){
 					document.getElementById(button_id).disabled  = true;
@@ -230,10 +220,10 @@
 				}*/
 				
 				//updateInput(field_id,product_id,maxQuantity);
-				runUpdate(product_id,currentVal,maxQuantity);
+				
 			}
 			
-			function updateInput(field_id,product_id,maxQuantity){
+			function addToCart(field_id,product_id,maxQuantity){
 				
 				var input_field = $("input[name='"+ field_id +"']");
 				var currentVal = parseInt(input_field.val());
@@ -264,7 +254,10 @@
 					}
 				}*/
 				
-				runUpdate(product_id,currentVal,maxQuantity);
+				webix.ajax().post("process/add_to_cart_process.php",{product_id:product_id,item_quantity:currentVal,max_quantity:maxQuantity},
+							function(text,data){
+							document.getElementById("resultAdd").innerHTML = text;
+				})
 			}
 			
 			
@@ -279,16 +272,6 @@
 				
 			}
 			
-			//Perform ajax action and return server response text
-			function runUpdate(product_id,currentValue,maxQuantity){
-				webix.ajax().post("process/updated_item_in_cart_process.php",{edit_product_id:product_id,new_quantity:currentValue,max_quantity:maxQuantity},
-							function(text,data){
-								
-								
-									location.reload();
-								
-						})
-			}
 		</script>
     </body>
 </html>
