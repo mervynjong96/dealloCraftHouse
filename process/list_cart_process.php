6c44aae64e11b5eb1373e71f6010baba71d2d464
jasonchan03 <jctaurus503@gmail.com>
2017-11-06 20:26:21 +0800

    Added array to store product id

--- process/list_cart_process.php
+++ process/list_cart_process.php
@@ -19,9 +19,7 @@
 			$query = "SELECT cart_product.product_quantity,products.product_id,products.product_name,products.product_price,products.product_stockQty FROM cart_product, products WHERE cart_product.product_id = products.product_id
 			 AND cart_product.userid = '$userid' ";
 			$result = mysqli_query($conn, $query);
-			if($result->num_rows>0){
-				//echo "<table border='1'>";
-				
+			if($result->num_rows>0){				
 				echo "<div class='row'>
 					<div class='col-sm-1 col-xs-1 col-md-1 col-lg-1 cartHeader'>
 					</div>
@@ -54,13 +52,10 @@
 				while($result_products = mysqli_fetch_assoc($result)){
 					
 					$directory = "./assets/images/products/" . $result_products["product_id"];
-					$images = glob("$directory/*.{jpg,png,bmp}", GLOB_BRACE);
-					$product_image ="";
-					foreach($images as $image)
-					{
-						$product_image = $image;
-						break;
-					}
+					$product_image = "";
+					$img_dir          =   "./assets/images/products/" . $result_products["product_id"];
+					$product_image    =   glob("$img_dir/a.*", GLOB_BRACE)[0];
+					
 					echo "<div class='row'>";
 					echo "<div class='col-sm-1 col-xs-1 col-md-1 col-lg-1 alignCenter'> <input type='checkbox' name='checkbox_". $result_products["product_id"] . "' id='checkbox_". $result_products["product_id"] . "' value='" . $result_products["product_price"]*$result_products["product_quantity"] . "' onclick='sumTotal(this.value,this.id)'/> </div>";
 					
@@ -86,23 +81,16 @@
 						</div>
 						<div class='col-sm-2 col-xs-2 col-md-2 col-lg-2 alignCenter'>" . number_format((float)$result_products["product_price"]*$result_products["product_quantity"],2,'.','') . "</div>
 						<div class='col-sm-1 col-xs-1 col-md-1 col-lg-1 alignCenter'><a href='#' onclick='removeItem(" . $result_products['product_id'] .")'><span class='glyphicon glyphicon-remove'></span></a></div>";
-					
-					/*echo "<tr>
-								<td> <input type='checkbox' name='". $result_products["product_id"] . "' value='" . $result_products["product_price"]*$result_products["product_quantity"] . "'/> </td>
-								<td> <img class='list-group-image productimg' src='" .  $product_image . "' alt='Picture of" . $result_products["product_name"] . "' /> </td>
-								<td>" . $result_products["product_name"] . "</td>
-								<td>" . $result_products["product_price"]*$result_products["product_quantity"] . "</td>
-								<td>" . $result_products["product_quantity"] . "</td>
-								<td> <span class='glyphicon glyphicon-remove'></span> </td>
-							</tr>";*/
+			
 					echo "</div> <hr/>";	
-					$product_image="";
+					
+					$selected_products[] = $result_products["product_id"];
+					
 				}
-				//echo "</table>";
 				
-				
+				$selected_products = json_encode($selected_products);
 				echo "	<p class='alignRight'> Total price: <span id='total_price'></span> </p>
-				<p class='alignRight'> <a class='btn btn-success' style='margin-right:10px;' onClick=''>Checkout</a> </p>";
+				<p class='alignRight'> <a class='btn btn-success' style='margin-right:10px;' onclick='checkSelectedItem($selected_products)'>Checkout</a> </p>";
 			}else{
 				
 				echo "Something goes wrong, please try again later";
