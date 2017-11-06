c001ab0ae9ac667429fbfc93b47f5ace3980e43f
jasonchan03 <jctaurus503@gmail.com>
2017-11-07 00:07:46 +0800

    Added update query for remove and edit for buyers

--- process/remove_item_from_cart_process.php
+++ process/remove_item_from_cart_process.php
@@ -11,15 +11,27 @@
 		
         $remove_product_id = mysqli_real_escape_string($conn, $_POST["remove_product_id"]);
       	$userid = mysqli_real_escape_string($conn, $_SESSION["login_user"]);
-		
+		$product_table = "products";
 		$sql_table = "cart_product";
-		
-		$query = "DELETE FROM $sql_table WHERE userid = '$userid' AND product_id='$remove_product_id'";
-		$result = mysqli_query($conn, $query);
+		$query  = "SELECT product_quantity FROM $sql_table WHERE product_id = '$remove_product_id'";
+		$result = mysqli_query($conn,$query);
+		if(mysqli_affected_rows($conn)>0){
+			$item_quantity = mysqli_fetch_assoc($result);
+			$query = "DELETE FROM $sql_table WHERE userid = '$userid' AND product_id='$remove_product_id'";
+			$result = mysqli_query($conn, $query);
 
-		if(mysqli_affected_rows($conn) > 0) {
-			echo "Remove item successfully";
-			
+			if(mysqli_affected_rows($conn) > 0) {
+				$stock_quantity = $item_quantity["product_quantity"];
+				$query = "UPDATE $product_table SET product_stockQty = product_stockQty + '$stock_quantity', product_qtyOnHold = product_qtyOnHold - '$stock_quantity' WHERE product_id='$remove_product_id'";
+				$result = mysqli_query($conn,$query);
+				if(mysqli_affected_rows($conn)>0){
+
+					echo "Remove item successfully";
+
+				}else{
+					echo mysqli_error($conn);
+				}
+			}
 		}
 		
 		mysqli_close($conn);
--- process/updated_item_in_cart_process.php
+++ process/updated_item_in_cart_process.php
@@ -14,26 +14,41 @@
 		$new_quantity = mysqli_real_escape_string($conn,$_POST["new_quantity"]);
 		$max_quantity = mysqli_real_escape_string($conn,$_POST["max_quantity"]);
 		$sql_table = "cart_product";
+		$product_table = "products";
 		
-		if($new_quantity < 1){
-			$_SESSION["invalid_message"] = "Item quantity must be at least 1";
-			$query = "UPDATE $sql_table SET product_quantity='1' WHERE userid = '$userid' AND product_id='$edit_product_id'";
-		}else if($new_quantity > $max_quantity){
-				$_SESSION["invalid_message"] = "Item quantity has exceed the maximum quantity. The maximum quantity has taken.";
-				$query = "UPDATE $sql_table SET product_quantity='$max_quantity' WHERE userid = '$userid' AND product_id='$edit_product_id'";
-		}else{
-			$query = "UPDATE $sql_table SET product_quantity='$new_quantity' WHERE userid = '$userid' AND product_id='$edit_product_id'";
-			unset($_SESSION["invalid_message"]);
-		}
-		
-		$result = mysqli_query($conn, $query);
+		$query  = "SELECT product_quantity FROM $sql_table WHERE product_id = '$edit_product_id'";
+		$result = mysqli_query($conn,$query);
+		if(mysqli_affected_rows($conn)>0){
+			$old_quantity = mysqli_fetch_assoc($result);
+			if($new_quantity < 1){
+				$_SESSION["invalid_message"] = "Item quantity must be at least 1";
+				$query = "UPDATE $sql_table SET product_quantity='1' WHERE userid = '$userid' AND product_id='$edit_product_id'";
+			}else if($new_quantity > $max_quantity){
+					$_SESSION["invalid_message"] = "Item quantity has exceed the maximum quantity. The maximum quantity has taken.";
+					$query = "UPDATE $sql_table SET product_quantity='$max_quantity' WHERE userid = '$userid' AND product_id='$edit_product_id'";
+			}else{
+				$query = "UPDATE $sql_table SET product_quantity='$new_quantity' WHERE userid = '$userid' AND product_id='$edit_product_id'";
+				unset($_SESSION["invalid_message"]);
+			}
 
-		if(mysqli_affected_rows($conn) > 0) {
-			if($new_quantity >=1 && $new_quantity <= $max_quantity){
-				echo "Update item quantity successfully";
+			$result = mysqli_query($conn, $query);
+
+			if(mysqli_affected_rows($conn) > 0) {
+				$update_quantity = $new_quantity - $old_quantity["product_quantity"];
+				$query = "UPDATE $product_table SET product_stockQty = product_stockQty + '$update_quantity', product_qtyOnHold = product_qtyOnHold - '$update_quantity' WHERE product_id='$edit_product_id'";
+				$result = mysqli_query($conn,$query);
+				if(mysqli_affected_rows($conn)>0){
+					if($new_quantity >=1 && $new_quantity <= $max_quantity){
+						echo "Update item quantity successfully";
+					}
+				}else{
+					echo mysqli_error($conn);
+				}
+			}else{
+				echo "Something goes wrong, Please try again later";
 			}
 		}else{
-			echo "Something goes wrong, Please try again later";
+			echo mysqli_error($conn);
 		}
 		
 		mysqli_close($conn);
