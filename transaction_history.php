1583f78ffea4bac64ee2fc53ab547415ccb1082f
professorgene <werewolf.xeno@yahoo.com>
2017-11-07 21:07:16 +0800

    added newly exported database
    updated database structure to include decimal places for specific values

--- transaction_history.php
+++ transaction_history.php
@@ -35,7 +35,7 @@
         <?php
             require "process/db_conn.php";
 
-            $query = "SELECT user_transactions.transaction_id, user_transactions.date_paid, user_transactions.total_amount, transaction_products.product_id, products.product_name, transaction_products.quantity, transaction_products.price FROM user_transactions LEFT JOIN transaction_products ON user_transactions.transaction_id = transaction_products.transaction_id LEFT JOIN products ON transaction_products.product_id = products.product_id WHERE user_transactions.userid = '$_SESSION[login_user]'";
+            $query = "SELECT user_transactions.transaction_id, user_transactions.date_paid, user_transactions.total_amount, transaction_products.product_id, products.product_name, transaction_products.quantity, transaction_products.price FROM user_transactions LEFT JOIN transaction_products ON user_transactions.transaction_id = transaction_products.transaction_id LEFT JOIN products ON transaction_products.product_id = products.product_id WHERE user_transactions.userid = '$_SESSION[login_user]' ORDER BY user_transactions.transaction_id DESC";
             $result = mysqli_query($conn, $query);
             $rows = array();
             while($i = mysqli_fetch_assoc($result)) {
@@ -52,14 +52,15 @@
                     container: "history",
                     view: "datatable",
                     columns: [
-                        { id:"transaction_id", header:"Transaction ID", width: 150 },
+                        { id:"transaction_id", header:"Transaction ID", width: 125 },
                         { id:"date_paid", header:"Date of Purchase", width: 175, format:webix.Date.dateToStr("%H:%i - %m/%d/%Y") },
-                        { id:"product_name", header:"Products Purchased", width: 500 },
-                        { id:"quantity", header:"Quantity", width: 75 },
-                        { id:"price", header:"Price", width: 150, format:function(value){ 
+                        { id:"product_id", header:"Product ID", width: 100 },
+                        { id:"product_name", header:"Product Purchased", width: 400 },
+                        { id:"quantity", header:"Quantity", width: 100 },
+                        { id:"price", header:"Price", width: 100, format:function(value){ 
                             value = "RM " + value;
                             return value; } },
-                        { id:"total_amount", header:"Total Cost", width: 150, /* format:webix.i18n.priceFormat */ format:function(value){ 
+                        { id:"total_amount", header:"Total Cost", width: 100, /* format:webix.i18n.priceFormat */ format:function(value){ 
                             value = "RM " + value;
                             return value; }
                         }
